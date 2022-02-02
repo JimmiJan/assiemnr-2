7eacce4f56d88cf9d9872958f310c4401f48d227
JimmiJan <96634490+JimmiJan@users.noreply.github.com>
2022-02-02 11:45:33 +0500

    Add files via upload

---
+++ src/components/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Style.css"
+
+
+
+const Name =()=>{
+    <div className="hae">
+
+    
+    <div className="header">
+        <div>
+                   <h3>
+                       IceCreame Shop
+                   </h3>
+        </div>
+        <div>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    </div>
+    </div>
+}
+export default Name
